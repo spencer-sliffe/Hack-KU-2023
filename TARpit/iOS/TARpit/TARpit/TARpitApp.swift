3e66faa1eac053fac6074b1e391a9f9f36853058
Spencer Sliffe <spenceronw@ku.edu>
2023-04-14 20:26:57 -0500

    just created the swift project

---
+++ TARpit/iOS/TARpit/TARpit/TARpitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TARpitApp.swift
+//  TARpit
+//
+//  Created by Spencer SLiffe on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TARpitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
