1d3581875dc27e477638889c33b1b19ceec9c37f
Spencer Sliffe <spenceronw@ku.edu>
2023-04-15 12:46:35 -0500

    HomePageView update

--- TARpit/iOS/TARpit/TARpit/Views/HomePageView.swift
+++ TARpit/iOS/TARpit/TARpit/Views/HomePageView.swift
@@ -7,43 +7,39 @@
 
 import Foundation
 import SwiftUI
-
 struct HomePageView: View {
     @State private var selectedTab = 2
     @ObservedObject var authViewModel = AuthViewModel()
     
     var body: some View {
-        ZStack {
-            LinearGradient(
-                gradient: Gradient(colors: [.yellow, .black]),
-                startPoint: .topLeading,
-                endPoint: .bottomTrailing
-            )
-            .edgesIgnoringSafeArea(.all)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            
-            NavigationView {
-                ZStack {
-                    if !authViewModel.isAuthenticated {
-                        AuthView(authViewModel: authViewModel)
-                    } else {
-                        TabView(selection: $selectedTab) {
-                            
-                            AccountView(authViewModel: authViewModel)
-                                .tag(0)
-                            TarPitView()
-                                .tag(1)
-                        }
-                        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                        .navigationBarHidden(true)
-                    }
-                }
+        NavigationView {
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [.yellow, .black]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
                 .edgesIgnoringSafeArea(.all)
+                
+                if !authViewModel.isAuthenticated {
+                    AuthView(authViewModel: authViewModel)
+                } else {
+                    TabView(selection: $selectedTab) {
+                        AccountView(authViewModel: authViewModel)
+                            .tag(0)
+                        TarPitView()
+                            .tag(1)
+                    }
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                    .navigationBarHidden(true)
+                }
             }
-            .navigationViewStyle(StackNavigationViewStyle())
+            .edgesIgnoringSafeArea(.all)
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
+
 struct HomePageView_Previews: PreviewProvider {
     static var previews: some View {
         HomePageView()
